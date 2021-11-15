2ccc73348f6427bb12b18df2486db56065640bee
Eusebio Justo Cerda <justoejc@gmail.com>
2021-11-15 08:48:22 -0300

    ejercicio corregido

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -103,11 +103,13 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
-  suma = 0;
-  for(var i = 0; i < numeros length; i++) {
-    suma = suma + numeros[i];
+  var maximo = numeros[0]
+  for(var i = 1; i < numeros.length; i++) {
+    if(numeros[i] > maximo) {
+      maximo = numeros[i];
+    }
   }
-  return suma;
+  return maximo;
 }
 
 
