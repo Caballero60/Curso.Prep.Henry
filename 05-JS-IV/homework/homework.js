c0724763420893377969c51c119ad4e539acdb13
Eusebio Justo Cerda <justoejc@gmail.com>
2021-12-03 21:41:21 -0300

    Septimo ejercicio

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -72,6 +72,12 @@ function tieneEmail(usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  if(usuario['email']) {
+    return  true;
+  }
+  else {
+    return false;
+  } 
 }
 
 
